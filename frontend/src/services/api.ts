52b4a940223fb1884ee2ac54e60e5c1da8c9cb60
Prajwal Prakash Kumar <prajwalp@ivoyant.com>
2026-04-22 18:19:58 +0530

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   
     baseQuery: fetchBaseQuery({
-         baseUrl: import.meta.env.VITE_API_URL,,
+         baseUrl: import.meta.env.VITE_API_URL,
         prepareHeaders: (headers) => {
             const token = localStorage.getItem("token");
 
