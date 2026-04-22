d3cd0d37e62467c911c5233e2be1b5eab509965c
Prajwal Prakash Kumar <prajwalp@ivoyant.com>
2026-04-22 18:27:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,13 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const app = express();
 const connectDB = require("./src/config/database");
 
-connectDB();
-
-const cors = require("cors");
+const app = express();
 
 app.use(
   cors({
-    origin: [
-      "https://simple-practice.netlify.app",
-    ],
+    origin: ["https://simple-practice.netlify.app"],
     credentials: true,
   })
 );
@@ -23,6 +18,18 @@ app.use(express.json());
 app.use("/api/auth", require("./src/routes/authRoutes"));
 app.use("/api/posts", require("./src/routes/postRoutes"));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB(); 
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+
+  } catch (error) {
+    console.error(" Failed to connect DB:", error);
+    process.exit(1); 
+  }
+};
+
+startServer();
