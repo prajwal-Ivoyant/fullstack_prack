0d148679c1177d25867a14b349fcf74357e7e85b
Prajwal Prakash Kumar <prajwalp@ivoyant.com>
2026-04-22 17:37:40 +0530

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-    reducerPath: "api",
+    baseUrl: import.meta.env.VITE_API_URL,
     baseQuery: fetchBaseQuery({
         baseUrl: "http://localhost:5000/api",
         prepareHeaders: (headers) => {
