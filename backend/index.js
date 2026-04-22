b1985ac6a141e0e6dcb6378fb4cf2ee8dd4f2b77
Prajwal Prakash Kumar <prajwalp@ivoyant.com>
2026-04-22 19:32:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,12 @@ const connectDB = require("./src/config/database");
 
 const app = express();
 
-app.use(
+
+
+const startServer = async () => {
+  try {
+    await connectDB(); 
+    app.use(
   cors({
     origin: ["https://glowing-panda-adf795.netlify.app/"],
     credentials: true,
@@ -17,10 +22,6 @@ app.use(express.json());
 
 app.use("/api/auth", require("./src/routes/authRoutes"));
 app.use("/api/posts", require("./src/routes/postRoutes"));
-
-const startServer = async () => {
-  try {
-    await connectDB(); 
 
     app.listen(process.env.PORT, () => {
       console.log(`Server running on port ${process.env.PORT}`);
